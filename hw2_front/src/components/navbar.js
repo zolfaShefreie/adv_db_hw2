2b6b16bfaa7e445af9d711b94fc27431fec063e5
zolfaShefreie <z.shefreie@gmail.com>
2021-11-17 08:37:28 +0330

    add router and navbar

---
+++ hw2_front/src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link, BrowserRouter, Route, Switch} from "react-router-dom";
+
+class Navbar extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <nav className="navbar dark">
+                <ul className="nav justify-content-end">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/user/form/">Add User</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/user/form/">Users</Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+export default Navbar;
---
+++ hw2_front/src/components/routers.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { Route, Routes, useHistory, BrowserRouter } from 'react-router-dom';
+
+
+export default function Routers() {
+    return (
+
+        <Routes>
+            {/*<Route exact path="/user/form/" render={() => <Login/>}/>*/}
+            {/*<Route exact path="/user/list/" render={() => <SignUp/>}/>*/}
+            {/*<Route exact path="/user/info/" render={() => <Info/>}/>*/}
+
+        </Routes>
+    );
+}
